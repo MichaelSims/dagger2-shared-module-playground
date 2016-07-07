dfd3446548883565c2eeb17ec5e3637161141d3b
Michael Sims <msims@mercuryintermedia.com>
2016-07-07 18:56:02 -0500

    Add a shared activity to shared module

--- app/src/main/java/com/example/dagger2sharedmoduleplayground/MainActivity.java
+++ app/src/main/java/com/example/dagger2sharedmoduleplayground/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.dagger2sharedmoduleplayground;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 import com.example.dagger2sharedmoduleplayground.services.StringService;
+import com.example.shared.view.SharedActivity;
 
 import javax.inject.Inject;
 
@@ -19,5 +22,13 @@ public class MainActivity extends AppCompatActivity {
         TextView textView = (TextView) findViewById(R.id.the_best_string);
         //noinspection ConstantConditions
         textView.setText(stringService.getTheBestString());
+        View button = findViewById(R.id.go_to_shared_activity);
+        //noinspection ConstantConditions
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SharedActivity.class));
+            }
+        });
     }
 }
---
+++ shared/src/main/java/com/example/shared/view/SharedActivity.java
@@ -0,0 +1,35 @@
+package com.example.shared.view;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+import com.example.shared.R;
+
+public class SharedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shared);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        //noinspection ConstantConditions
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        TextView textView = (TextView) findViewById(R.id.shared_text_view);
+        //noinspection ConstantConditions
+        textView.setText("Testing");
+    }
+
+}
